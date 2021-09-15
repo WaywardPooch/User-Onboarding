0fde3869729ff120ad2d8f31f5b1475ac785bdf9
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-15 12:24:29 -0700

    changed folder structure; added Header component

---
+++ user-onboarding-app/src/components/App/App.js
@@ -0,0 +1,14 @@
+// Import Components
+import Header from "../Header/Header";
+// Import Styles
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ user-onboarding-app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.less";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>TEST</h1>
+    </header>
+  );
+};
+
+export default Header;
